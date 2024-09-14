3a01e7a631513d1cdda4a450a749fd62b2135918
Demzzy <155068004+Bbn-hub@users.noreply.github.com>
2024-09-15 00:34:56 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,17 +17,17 @@ global.ytname = "YT: Jean-parker-tech"
 global.socialm = "GitHub: Jeanparker100"
 global.location = "Togo, maritime, Lomé"
 
-global.ownernumber = 2347048641826;  //creator number
+global.ownernumber = '2347048641826'  //creator number
 global.ownername = '𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛' //owner name
 global.botname = '𝙿𝙰𝚁𝙺𝚈-𝙱𝚄𝙶-𝙱𝙾𝚃' //name of the bot
 
 //sticker details
 global.packname = '¹⁴³ ᴬˡᵉᵐᵃᵖ 🧸💜'
-global.author = '𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛'
+global.author = 'Demzzy 𝙿𝚊𝚛𝚔𝚎𝚛'
 
 //console view/theme
 global.themeemoji = '🩵'
-global.wm = "𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛 in©"
+global.wm = "Demzzy 𝙿𝚊𝚛𝚔𝚎𝚛 in©"
 
 //theme link
 global.link = 'https://chat.whatsapp.com/G8aBi0Y8KwIEaQr6KJnylc'
